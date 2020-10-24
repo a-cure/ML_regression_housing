30e0cd354c9782a415376d01c2ec525634e7e920
alexiscure <a.cure@cafeyn.co>
2020-10-24 20:13:33 +0200

    initialize the project

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ML_regression_housing
+
+This project is a quick overview of some of the steps of a datasciense projet 
---
+++ src/utils.py
@@ -0,0 +1,38 @@
+import os
+import tarfile
+from six.moves import urllib
+from zlib import crc32
+import numpy as np
+
+
+def fetch_and_save_tgz(housing_url, housing_path):
+    os.makedirs(housing_path, exist_ok=True)
+    tgz_path = os.path.join(housing_path, "housing.tgz")
+    urllib.request.urlretrieve(housing_url, tgz_path)
+    housing_tgz = tarfile.open(tgz_path)
+    housing_tgz.extractall(path=housing_path)
+    housing_tgz.close()
+    
+
+
+def load_cv_file(housing_path, file_name):
+    csv_path = os.path.join(housing_path,file_name)
+    return pd.read_csv(csv_path)
+
+
+# For illustration only. Sklearn has train_test_split()
+def split_train_test(data, test_ratio):
+    shuffled_indices = np.random.permutation(len(data))
+    test_set_size = int(len(data) * test_ratio)
+    test_indices = shuffled_indices[:test_set_size]
+    train_indices = shuffled_indices[test_set_size:]
+    return data.iloc[train_indices], data.iloc[test_indices]
+
+
+def test_set_check(identifier, test_ratio):
+    return crc32(np.int64(identifier)) & 0xffffffff < test_ratio * 2**32
+
+def split_train_test_by_id(data, test_ratio, id_column):
+    ids = data[id_column]
+    in_test_set = ids.apply(lambda id_: test_set_check(id_, test_ratio))
+    return data.loc[~in_test_set], data.loc[in_test_set]
